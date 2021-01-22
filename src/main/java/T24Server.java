a4f3036409415de6eca2072f0815c24845b35a6a
jonmersha <jonmersa@gmail.com>
2021-01-23 00:22:35 +0300

    first commitwww

---
+++ src/main/java/T24Server.java
@@ -0,0 +1,17 @@
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class T24Server {
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss=new ServerSocket(8080);
+        Socket s=ss.accept();
+        DataOutputStream dout=new DataOutputStream(s.getOutputStream());
+        try {
+            dout.writeUTF("Hello Server");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
