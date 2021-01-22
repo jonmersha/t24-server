8bcb88e17fbc3cb7694d7a4ad145c7739593fb36
jonmersha <jonmersa@gmail.com>
2021-01-23 00:31:36 +0300

    Second Commit

--- src/main/java/T24Server.java
+++ src/main/java/T24Server.java
@@ -5,11 +5,15 @@ import java.net.Socket;
 
 public class T24Server {
     public static void main(String[] args) throws IOException {
-        ServerSocket ss=new ServerSocket(8080);
+        ServerSocket ss=new ServerSocket(80);
         Socket s=ss.accept();
         DataOutputStream dout=new DataOutputStream(s.getOutputStream());
+        int x=0;
         try {
+            while(x<500){
             dout.writeUTF("Hello Server");
+            x++;
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
